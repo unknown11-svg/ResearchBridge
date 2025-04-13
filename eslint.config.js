f15f9d2db41c1815cd3ffe5793c7ac23a534620c
unknown11_svg <2434088@students.wits.ac.za>
2025-04-13 19:42:11 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-// eslint.config.js
+// eslint.config.cjs
 const { FlatCompat } = require('@eslint/eslintrc');
 const js = require('@eslint/js');
 const tsParser = require('@typescript-eslint/parser');
@@ -8,27 +8,33 @@ const reactPlugin = require('eslint-plugin-react');
 const compat = new FlatCompat();
 
 module.exports = [
+  // JavaScript recommended rules
   js.configs.recommended,
+
+  // TypeScript recommended rules (converted to flat config)
   ...compat.extends('plugin:@typescript-eslint/recommended'),
+
   {
-    plugins: {
-      react: reactPlugin,
-      '@typescript-eslint': tsPlugin
-    },
+    name: 'custom-config',
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2021,
       sourceType: 'module',
       globals: {
-        module: 'readable'
+        module: 'readonly'
       }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin
     },
     rules: {
       'no-console': 'warn',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
+      'no-undef': 'off',
       '@typescript-eslint/no-require-imports': 'off',
-      'no-undef': 'off'
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off'
     },
     settings: {
       react: {
